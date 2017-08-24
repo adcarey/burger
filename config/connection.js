bdc88bde83581a8f15cc2b7409ae5b37195daa29
adcarey <ad.carey1990@gmail.com>
2017-08-24 06:11:21 -0600

    updates

--- config/connection.js
+++ config/connection.js
@@ -11,12 +11,6 @@ if (process.env.JAWSDB_URL) {
   	user: "root",
   	password: "1234",
   	database: "burgers_db"
-	// connection = mysql.createConnection({
-	// 	host: "	uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	// 	user: "	q00jes3wjnqst8r9",
-	// 	password: "uffxfp4509vm3qch",
-	// 	database: "burgers_db"
-	// });
 });
 };
 
