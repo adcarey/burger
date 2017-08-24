cdf4f0ba48580da9de32c0bf2edb668cbc926d1c
adcarey <ad.carey1990@gmail.com>
2017-08-23 22:04:58 -0600

    final

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,20 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 	connection = mysql.createConnection({
-		host: "	uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-		user: "	q00jes3wjnqst8r9",
-		password: "uffxfp4509vm3qch",
-		database: "burgers_db"
-	});
+  	port: 3306,
+  	host: "localhost",
+  	user: "root",
+  	password: "1234",
+  	database: "burgers_db"
+	// connection = mysql.createConnection({
+	// 	host: "	uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	// 	user: "	q00jes3wjnqst8r9",
+	// 	password: "uffxfp4509vm3qch",
+	// 	database: "burgers_db"
+	// });
 };
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "1234",
-//   database: "burgers_db"
-// });
+
 
 // Make connection.
 connection.connect(function(err) {
